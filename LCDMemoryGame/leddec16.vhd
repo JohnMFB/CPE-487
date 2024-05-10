8d9b336c2f96841fb65771d564525c0b384d2bfe
John Li <122575719+JohnMFB@users.noreply.github.com>
2024-05-10 11:56:15 -0400

    Update leddec16.vhd

--- LCDMemoryGame/leddec16.vhd
+++ LCDMemoryGame/leddec16.vhd
@@ -42,7 +42,7 @@ BEGIN
     anode <= "11111110" WHEN dig = "000" ELSE -- 0
              "11111101" WHEN dig = "001" ELSE -- 1
              "11111011" WHEN dig = "010" ELSE -- 2
-             "11110111" WHEN dig = "011" ELSE -- 3
+             -- "11110111" WHEN dig = "011" ELSE -- 3
 --	         "11101111" WHEN dig = "100" ELSE -- 4
 --	         "11011111" WHEN dig = "101" ELSE -- 5 
 --	         "10111111" WHEN dig = "110" ELSE -- 6
